66b502d62bdbeb756ce55df2c7d035c87a4239b2
muradrahmanzada <muradrahmanzada@gmail.com>
2023-01-21 13:31:44 +0400

    initialize the repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+import "./styles/reset.css";
+import "./styles/variable.css";
+import "./styles/global.css";
+
+function App() {
+  return <div>Interactive Comments</div>;
+}
+
+export default App;
